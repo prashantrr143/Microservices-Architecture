696b70e190c63c09a2f6091e9c0db961774dfde3
Prashant Kumar Singh <prashant.siet153@gmail.com>
2018-06-27 22:08:21 +0530

    Initial Commit Inventory Management Service

---
+++ InventoryManagementService/src/main/java/in/prashant/microservices/inventory/InventoryManagementServiceApplication.java
@@ -0,0 +1,12 @@
+package in.prashant.microservices.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagementServiceApplication.class, args);
+	}
+}
