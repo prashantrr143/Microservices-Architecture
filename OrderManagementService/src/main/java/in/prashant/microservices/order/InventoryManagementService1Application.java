0c3853e1bf697c8b6414e905a92ee03dd27b0bec
Prashant Kumar Singh <prashant.siet153@gmail.com>
2018-06-27 22:10:53 +0530

    Intitial Commit Order Management Service

---
+++ OrderManagementService/src/main/java/in/prashant/microservices/order/InventoryManagementService1Application.java
@@ -0,0 +1,12 @@
+package in.prashant.microservices.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagementService1Application.class, args);
+	}
+}
