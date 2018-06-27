60e5477de751db92e15a30fe766f45b5f07dbd71
Prashant Kumar Singh <prashant.siet153@gmail.com>
2018-06-27 22:03:57 +0530

    Initial Commit Eureka Server

---
+++ EurekaServiceRegistryServer/src/main/java/in/prashant/microservices/config/ZuulProxyServer1Application.java
@@ -0,0 +1,12 @@
+package in.prashant.microservices.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulProxyServer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulProxyServer1Application.class, args);
+	}
+}
