59afd919715a91f3b2068cd4919d0c9e5cf610e7
Prashant Singh <prasingh26@publicisgroupe.net>
2018-07-22 00:33:41 +0530

    Updaed to add for Product Service

--- InventoryManagementService/src/main/java/com/onlinestore/microservices/inventrory/service/impl/ProductServiceImpl.java
+++ InventoryManagementService/src/main/java/com/onlinestore/microservices/inventrory/service/impl/ProductServiceImpl.java
@@ -1,7 +1,10 @@
 package com.onlinestore.microservices.inventrory.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.onlinestore.microservices.inventrory.service.ProductService;
 
+@Service
 public class ProductServiceImpl implements ProductService {
 
 }
