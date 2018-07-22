e108a0788503644185f64c95317b1f9babd4ccd5
prasingh26 <prasingh26@WKWIN1194166.global.publicisgroupe.net>
2018-07-22 14:28:01 +0530

    Updating module structure

--- ZuulProxyServer/src/main/java/in/prashant/microservices/config/zuul/ZuulProxyServerApplication.java
+++ ZuulProxyServer/src/main/java/in/prashant/microservices/config/zuul/ZuulProxyServerApplication.java
@@ -2,8 +2,10 @@ package in.prashant.microservices.config.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ZuulProxyServerApplication {
 
 	public static void main(String[] args) {
